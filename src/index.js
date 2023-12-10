9fdc34a4d851650c08e84222d0915422516e7b85
Sameer <YOUR_EMAIL>
2023-12-10 18:18:37 +0530

    made changes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from './signup';
 import Login from './Login';
-import LoginSucces from './LoginSucces';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -13,7 +13,7 @@ root.render(
     <Routes>
     <Route path="/" element={<Signup/>}/>
     <Route path="/login" element={<Login/>}/>
-    <Route path="/loginsuccess" element={<LoginSucces/>}/>
+    
     
   </Routes>
   </BrowserRouter>
